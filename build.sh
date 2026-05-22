c1058995617b53a2f9f3feb6479e6d257b3f2ce7
root <root@ip-172-31-3-117.ec2.internal>
2026-05-22 11:14:26 +0000

    updated

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ docker stop react
 docker rm react
 
 #building a image:
-docker build -t test .
+docker build -t summer .
 
 #running a container from the created image:
-docker run -d -it --name react -p 80:80 test
+docker run -d -it --name react -p 80:80 summer
 
 #pushing the image to dockerhub:
-docker tag test srilekhadevops10/react-app-image
-docker push srilekhadevops10/react-app-image
+docker tag summer srilekhadevops10/summer-app
+docker push srilekhadevops10/summer-app
