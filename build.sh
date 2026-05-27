12eb1001cf485429ec06003f8d54134b79d77f92
root <root@ip-172-31-0-9.ec2.internal>
2026-05-27 04:28:43 +0000

    updated files

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
 #stopping existing container:
-docker stop react
-docker rm react
+docker stop web
+docker rm web
 
 #building a image:
-docker build -t summer .
+docker build -t flower .
 
 #running a container from the created image:
-docker run -d -it --name react -p 80:80 summer
+docker run -d -it --name web -p 90:80 flower
 
 #pushing the image to dockerhub:
-docker tag summer srilekhadevops10/summer-app
-docker push srilekhadevops10/summer-app
+docker tag flower srilekhadevops10/flower
+docker push srilekhadevops10/flower
